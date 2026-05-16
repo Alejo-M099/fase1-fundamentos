cb6b1e07fa0c32fc1e43b109d1209c015c6a0cbb
Alejandro Miranda <jamccali1999@gmail.com>
2026-05-16 15:53:29 -0500

    Repaso semana 1: ejercicios 1 al 4 de JavaScript

--- REPASO/repaso.js
+++ REPASO/repaso.js
@@ -1,7 +1,26 @@
 const nombre = document.querySelector('#titulo')
 const boton = document.querySelector('#btn-desarrollo')
+const lista = document.querySelector('#lista')
+const agregar = document.querySelector('#btn-agregar')
+const nuevoElemento = document.createElement('li')
+const contar = document.querySelector('#btn-contar')
+const contador = document.querySelector('#contador')
+
+let numero = 0
 
 boton.addEventListener('click', function() {
     nombre.textContent = 'Soy Desarrollador Web!'
+
+})
+
+agregar.addEventListener('click', function(){
+    nuevoElemento.textContent = 'Estoy aprendiendo JavaScript'
+    lista.appendChild(nuevoElemento)
+
+})
+
+contar.addEventListener('click', function(){
+    numero = numero + 1
+    contador.textContent = numero
 
 })
